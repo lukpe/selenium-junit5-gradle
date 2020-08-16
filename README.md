9f58b834f815801e5541d2ffb45146a1de4d4884
Luk Pe <luk.pe@outlook.com>
2020-08-16 15:05:54 +0200

    Add ability to run remote tests

--- README.md
+++ README.md
@@ -9,14 +9,15 @@ Selenium framework utilizing Junit5, built with Gradle
 ## how to run
 [test.properties]: src/main/resources/test.properties
 * `git clone https://github.com/lukpe/selenium-junit5-gradle.git`
-* `gradlew clean test -Dbrowser={browser}`
+* `gradlew clean test -Dbrowser={browser} -Dremote`
 
-##### configuration:
+## configuration
 * Supported browsers: `chrome` `firefox` `edge`
-* If no `-Dbrowser=` parameter then `browser` is taken from [test.properties] 
+* If no `-Dbrowser=` then `browser` is taken from [test.properties]
+* If no `-Dremote` (e.g. `http://localhost:4444/wd/hub`) then test will run locally
 * Page `url` & default `timeout` are also set in [test.properties]
 
-## highlights
+## main features
 * [JUnit5](https://junit.org/junit5/)
 * [Page Factory](https://github.com/SeleniumHQ/selenium/wiki/PageFactory)
 * [webdrivermanager](https://github.com/bonigarcia/webdrivermanager)
--- src/main/java/org/test/TestBase.java
+++ src/main/java/org/test/TestBase.java
@@ -5,9 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -15,41 +19,53 @@ public class TestBase {
     public WebDriver driver;
     public Properties prop;
 
-    public WebDriver initDriver(){
+    public WebDriver initDriver() {
         initProp();
 
         String browser = System.getProperty("browser");
-        if(browser == null){
-            browser = prop.getProperty("browser").toLowerCase();
+        if (browser.isEmpty()) {
+            browser = prop.getProperty("browser");
         }
+        browser = browser.toLowerCase();
 
-        switch (browser) {
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
-            default:
-                System.out.println("Invalid browser name, selecting Chrome");
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+        String remote = System.getProperty("remote");
+        if (remote.isEmpty()) {
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    System.out.println("Invalid browser name, selecting Chrome");
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+        } else {
+            DesiredCapabilities dc = new DesiredCapabilities();
+            dc.setBrowserName(browser);
+            try {
+                driver = new RemoteWebDriver(new URL(remote), dc);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
         long timeOut = Long.parseLong(prop.getProperty("timeout"));
         driver.manage().timeouts().implicitlyWait(timeOut, TimeUnit.SECONDS);
         return driver;
     }
 
-    private void initProp(){
-        try{
-        FileInputStream file = new FileInputStream(System.getProperty("user.dir") +
-                "\\src\\main\\resources\\test.properties");
+    private void initProp() {
+        try {
+            FileInputStream file = new FileInputStream(System.getProperty("user.dir") +
+                    "\\src\\main\\resources\\test.properties");
             prop = new Properties();
             prop.load(file);
         } catch (IOException e) {
